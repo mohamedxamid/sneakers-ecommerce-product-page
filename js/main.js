684bbcf2f5937a24d9c3a37344e39bc63c899404
Muhammad Xamidov <muhammadxamidov001@gmail.com>
2023-08-09 19:54:33 +0300

    Update active showcase img on thumbnail click JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const modifiers = {
+    imgThumbnailActive: 'img-showcase__thumbnail--active'
+}
+
+// Shopping Cart Modal
 const elSiteHeaderCartLink = document.querySelector('.js-site-header__cart-link');
 const elSiteHeaderCartModal= document.querySelector('.site-header__cart-modal');
 
@@ -9,3 +14,24 @@ if (elSiteHeaderCartLink) {
     })
 }
 
+
+// Image Showcase
+const elImgShowcaseActiveImg = document.querySelector('.img-showcase__active-img');
+const elsImgShowcaseThumbnailButton = document.querySelectorAll('.js-img-showcase-thumbnail-button');
+const elsImgThumbnail = document.querySelectorAll('.img-showcase__thumbnail');
+
+elsImgShowcaseThumbnailButton.forEach(function (elButton) {
+    elButton.addEventListener('click', function (evt) {
+        // Remove active state from all buttons
+        elsImgThumbnail.forEach(function (elImgThumbnail) {
+            elImgThumbnail.classList.remove(modifiers.imgThumbnailActive);
+        });
+
+        // Add active state to clicked button
+        elButton.parentElement.classList.add(modifiers.imgThumbnailActive);
+
+        // Update active img src accordingly
+        elImgShowcaseActiveImg.src = elButton.dataset.imgShowcaseBig;
+        elImgShowcaseActiveImg.setAttribute('srcset', `${elButton.dataset.imgShowcaseBig} 1x, ${elButton.dataset.imgShowcaseRetina} 2x`)
+    })
+})
