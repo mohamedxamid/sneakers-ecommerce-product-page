6e5ace80cc7500c8aa6a041eaa0b58c09c201242
Muhammad Xamidov <muhammadxamidov001@gmail.com>
2023-08-13 17:17:44 +0300

    Add Product Info side

--- js/main.js
+++ js/main.js
@@ -133,5 +133,26 @@ if (elLightboxControlPrev) {
         elImgLightboxActiveImg.setAttribute('srcset', `${elNextActiveItem.children[0].dataset.imgShowcaseBig} 1x, ${elNextActiveItem.children[0].dataset.imgShowcaseRetina} 2x`)
     });
 }
+
+
+// Product Count
+const elProductQty = document.querySelector('.product-info__quantity');
+const elProductQtyDecrementButton = document.querySelector('.js-product-quantity-decrement-button');
+const elProductQtyIncrementButton = document.querySelector('.js-product-quantity-increment-button');
+
+
+if (elProductQtyIncrementButton) {
+    elProductQtyIncrementButton.addEventListener('click', function () {
+        elProductQty.textContent = parseInt(elProductQty.textContent, 10) + 1;
     })
-})
+}
+
+if (elProductQtyDecrementButton) {
+    elProductQtyDecrementButton.addEventListener('click', function () {
+        const qty = parseInt(elProductQty.textContent, 10);
+        
+        if (qty > 0) {
+            elProductQty.textContent = parseInt(elProductQty.textContent, 10) - 1;
+        }
+    })
+}
